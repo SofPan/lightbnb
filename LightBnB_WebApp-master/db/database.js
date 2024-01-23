b5b64efcf1f122020856b63f9f32287883bd82da
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-01-23 11:11:32 -0500

    refactor getUserWithEmail

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -18,14 +18,11 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+  return pool.query(`SELECT * FROM users WHERE email = $1`, [email])
+    .then(result => {
+      return result.rows[0] || null;
+    })
+    .catch(err => console.log('getUserWithEmail error', err.message));
 };
 
 /**
