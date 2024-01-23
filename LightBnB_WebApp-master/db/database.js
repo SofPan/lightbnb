bf960b8670ea15548578e9a17d3c2f633e736785
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-01-23 12:28:05 -0500

    refactor getAllReservations

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -63,8 +63,24 @@ const addUser = function (user) {
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+const getAllReservations = function (guestId, limit = 10) {
+  const queryStatement = `
+    SELECT reservations.*, properties.*, avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON properties.id = property_id
+    JOIN property_reviews ON reservations.id = reservation_id
+    WHERE reservations.guest_id = $1
+    GROUP BY reservations.id, properties.id
+    ORDER BY start_date
+    LIMIT $2;
+  `;
+  const values = [guestId, limit];
+  return pool
+    .query(queryStatement, values)
+    .then(result => {
+      return result.rows;
+    })
+    .catch(err => console.log('getAllReservations error:', err.message));
 };
 
 /// Properties
